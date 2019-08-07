f216ebf90de35a3ff7fc72bca25c74668f2b8fb1
Phil Shin <49408402+phil-shin@users.noreply.github.com>
2019-08-07 16:15:41 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      Test - Ready
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Test 1
+      </div>
+    )
+  }
 }
 
 export default App;
