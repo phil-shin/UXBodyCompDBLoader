f421ad2a6b63c62bb89f9e2d24ed41674f6f7b6c
Phil Shin <49408402+phil-shin@users.noreply.github.com>
2019-08-08 16:12:31 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  // continually fetch data while Component is running
+  componentDidMount() {
+    this.interval = setInterval(() => this.getData(), 1000);
+  }
+
+  // stop data fetching when Component unmounts
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  // [C]reate database doc event handler
+
+  // [R]ead - Get all database data event handler
+
+  // [U]pdate database doc event handler
+
+  // [D]elete database doc event handler
+
   render() {
     return (
       <div>
-        Test 1
+        <div className="create-container">
+          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
+          <button type="submit" value="Submit" onClick={(e)=>{}}></button>
+        </div>
+        <div className="read-container">
+          <input type="text" placeholder="read" onChange={(e)=>{}}></input>
+          <button type="submit" value="Submit" onClick={(e)=>{}}></button>
+        </div>
+        <div className="update-container">
+          <input type="text" placeholder="update" onChange={(e)=>{}}></input>
+          <button type="submit" value="Submit" onClick={(e)=>{}}></button>
+        </div>
+        <div className="delete-container">
+          <input type="text" placeholder="delete" onChange={(e)=>{}}></input>
+          <button type="submit" value="Submit" onClick={(e)=>{}}></button>
+        </div>
       </div>
     )
   }
