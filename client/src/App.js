4b1b1ddb55fcf57591d0f7241b18e1a6cd803f18
Phil Shin <49408402+phil-shin@users.noreply.github.com>
2019-08-12 07:28:54 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,66 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data=[],
+      ftToAdd = null,
+      inToAdd = null,
+      lnWeightToAdd = null,
+      lnAthNameToAdd = null,
+      lnAthPosToAdd = null,
+      lnAthImgPathToAdd = null,
+      athWeightToAdd = null,
+      athAthNameToAdd = null,
+      athAthPosToAdd = null,
+      athAthImgPathToAdd = null,
+      blkWeightToAdd = null,
+      blkAthNameToAdd = null,
+      blkAthPosToAdd = null,
+      blkAthImgPathToAdd = null,
+      fieldToUpdate = null,
+      updateToApply = null,
+      ftToDelete = null,
+      inToDelete = null
     }
-  }
+  };
 
   // continually fetch data while Component is running
   componentDidMount() {
     this.interval = setInterval(() => this.getData(), 1000);
-  }
+  };
 
   // stop data fetching when Component unmounts
   componentWillUnmount() {
     clearInterval(this.interval);
-  }
+  };
 
   // [C]reate database doc event handler
+  createDoc = (feet, inches, lnWeight, lnAthName, lnAthPos, lnAthImgPath, 
+    athWeight, athAthName, athAthPos, athAthImgPath, 
+    blkWeight, blkAthName, blkAthPos, blkAthImgPath) => {
+      axios.post('http://localhost:3001/api/createData', {
+        feet: feet,
+        inches: inches,
+        leanWeight: lnWeight,
+        leanAthleteName: lnAthName,
+        leanAthletePosition: lnAthPos,
+        leanAthleteImagePath: lnAthImgPath,
+        athleticWeight: athWeight,
+        athleticAthleteName:athAthName,
+        athleticAthletePosition: athAthPos,
+        athleticAthleteImagePath: athAthImgPath,
+        bulkyWeight: blkWeight,
+        bulkyAthleteName: blkAthName,
+        bulkyAthletePosition: blkAthPos,
+        bulkyAthleteImagePath: blkAthImgPath
+      })
+  };
 
   // [R]ead - Get all database data event handler
+  getDataFromDB= () => {
+    axios.get('http://localhost:3001/api/getData')
+    .then((data) => this.setState({data : data}))
+    .catch((err) => console.log(err))
+  };
 
   // [U]pdate database doc event handler
 
