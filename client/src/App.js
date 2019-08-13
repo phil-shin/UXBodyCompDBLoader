6133c13c3dcdf92101c096f48c1aa004eae357a5
Phil Shin <49408402+phil-shin@users.noreply.github.com>
2019-08-13 07:35:18 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,32 @@ const axios = require('axios');
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data=[],
-      ftToAdd = null,
-      inToAdd = null,
-      lnWeightToAdd = null,
-      lnAthNameToAdd = null,
-      lnAthPosToAdd = null,
-      lnAthImgPathToAdd = null,
-      athWeightToAdd = null,
-      athAthNameToAdd = null,
-      athAthPosToAdd = null,
-      athAthImgPathToAdd = null,
-      blkWeightToAdd = null,
-      blkAthNameToAdd = null,
-      blkAthPosToAdd = null,
-      blkAthImgPathToAdd = null,
-      fieldToUpdate = null,
-      updateToApply = null,
-      ftToDelete = null,
-      inToDelete = null
-    }
+    this.state = { 
+      data: [],
+      ftToAdd: null,
+      inToAdd: null,
+      lnWeightToAdd: null,
+      lnAthNameToAdd: null,
+      lnAthPosToAdd: null,
+      lnAthImgPathToAdd: null,
+      athWeightToAdd: null,
+      athAthNameToAdd: null,
+      athAthPosToAdd: null,
+      athAthImgPathToAdd: null,
+      blkWeightToAdd: null,
+      blkAthNameToAdd: null,
+      blkAthPosToAdd: null,
+      blkAthImgPathToAdd: null,
+      fieldToUpdate: null,
+      updateToApply: null,
+      ftToDelete: null,
+      inToDelete: null
+    };
   };
 
   // continually fetch data while Component is running
   componentDidMount() {
-    this.interval = setInterval(() => this.getData(), 1000);
+    this.interval = setInterval(() => this.getDataFromDB(), 1000);
   };
 
   // stop data fetching when Component unmounts
@@ -90,21 +90,26 @@ class App extends Component {
     return (
       <div>
         <div className="create-container" style={{padding: '10px'}}>
-          <input type="text" placeholder="Feet" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="Inches" onChange={(e)=> {}}></input><br></br>
-          <input type="text" placeholder="Lean Weight (lbs)" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="Lean Athlete Name" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="Lean Athlete Position" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <input type="text" placeholder="create" onChange={(e)=> {}}></input>
-          <button type="submit" value="Submit" onClick={(e)=>{}}></button>
+          <input type="text" placeholder="Feet" onChange={(e)=> {this.setState({ftToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Inches" onChange={(e)=> {this.setState({inToAdd: e.target.value})}}></input><br></br>
+          <input type="text" placeholder="Lean Weight (lbs)" onChange={(e)=> {this.setState({lnWeightToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Lean Athlete Name" onChange={(e)=> {this.setState({lnAthNameToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Lean Athlete Position" onChange={(e)=> {this.setState({lnAthPosToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Lean Athlete Image Path" onChange={(e)=> {this.setState({lnAthImgPathToAdd: e.target.value})}}></input><br></br>
+          <input type="text" placeholder="Athletic Weight (lbs)" onChange={(e)=> {this.setState({athWeightToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Athletic Athlete Name" onChange={(e)=> {this.setState({athAthNameToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Athletic Athlete Position" onChange={(e)=> {this.setState({athAthPosToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Athletic Athlete Image Path" onChange={(e)=> {this.setState({athAthImgPathToAdd: e.target.value})}}></input><br></br>
+          <input type="text" placeholder="Bulky Weight (lbs)" onChange={(e)=> {this.setState({blkWeightToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Bulky Athlete Name" onChange={(e)=> {this.setState({blkAthNameToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Bulky Athlete Position" onChange={(e)=> {this.setState({blkAthPosToAdd: e.target.value})}}></input>
+          <input type="text" placeholder="Bulky Athlete Image Path" onChange={(e)=> {this.setState({blkAthImgPathToAdd: e.target.value})}}></input>
+          <button type="submit" value="Submit" onClick={(e)=>{this.createDoc(
+            this.state.ftToAdd, this.state.inToAdd, 
+            this.state.lnWeightToAdd, this.state.lnAthNameToAdd, this.state.lnAthPosToAdd, this.state.lnAthImgPathToAdd, 
+            this.state.athWeightToAdd, this.state.athAthNameToAdd, this.state.athAthPosToAdd, this.state.athAthImgPathToAdd, 
+            this.state.blkWeightToAdd, this.state.blkAthNameToAdd, this.state.blkAthPosToAdd, this.state.blkAthImgPathToAdd
+          )}}></button>
         </div>
         <div className="read-container">
           <input type="text" placeholder="read" onChange={(e)=>{}}></input>
@@ -120,7 +125,7 @@ class App extends Component {
         </div>
       </div>
     )
-  }
-}
+  };
+};
 
 export default App;
